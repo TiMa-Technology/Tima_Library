5d587edad8d7e31db7882f2233a273a4052dbe3c
davelin18yufan <davelin30630@gmail.com>
2025-11-27 17:38:29 +0800

    [refactor] update packages and re config to mustiple entry point with config fix.

--- javascript/tsdown.config.js
+++ javascript/tsdown.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: ["src/main.ts"],
+  entry: {
+    main: "src/main.ts",
+    auth: "src/auth/index.ts",
+    browserUtils: "src/browserUtils/index.ts",
+    baseFunction: "src/baseFunction/index.ts",
+  },
   dts: true,
   outDir: "dist",
   format: "esm",
+  clean: true,
 });
